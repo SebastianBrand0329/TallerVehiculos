050c41e5a83a17cb14be35d227e505d3bfb7b679
SebastianBrand0329 <sebastianbrand046@outlook.com>
2022-10-10 16:39:15 -0500

    Initial Folder and entities

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TallerVehiculos.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions option): base(option)
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/Entidades/Vehiculo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallerVehiculos.Entidades
+{
+    public class Vehiculo
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Placa { get; set; }
+        public string Color { get; set; }
+        public int Modelo { get; set; }
+    }
+}
